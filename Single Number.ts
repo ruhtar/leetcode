344d3e8d5a935ae9e99fae49101318cc9fbcf759
ruhtar <arthur.amorim10@gmail.com>
2024-03-19 21:27:44 -0300

    comments

--- Single Number.ts
+++
@@ -1,7 +0,0 @@
-function singleNumber(nums: number[]): number {
-  for (let index = 0; index < nums.length; index++) {
-    const element = nums[index];
-  }
-}
-
-console.log(singleNumber([4, 1, 2, 1, 2]));
--- TwoSums/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using TwoSums;
-
-
-var result = Solution.TwoSum([2, 7, 11, 15], 9);
-foreach (var item in result)
-{
-    Console.WriteLine(item);
-}
-Console.ReadLine();
