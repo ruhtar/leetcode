53e7cbee417eecce2dba07558bb8a2bb96790a69
ruhtar <arthur.amorim10@gmail.com>
2024-03-19 21:27:53 -0300

    comments

---
+++ Merge Two Sorted Lists.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {}
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
---
+++ Single Number.ts
@@ -0,0 +1,7 @@
+function singleNumber(nums: number[]): number {
+  for (let index = 0; index < nums.length; index++) {
+    const element = nums[index];
+  }
+}
+
+console.log(singleNumber([4, 1, 2, 1, 2]));
