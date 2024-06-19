0afb3773a8b0202ac2640f8d8b8afd1bb2643519
ruhtar <arthur.amorim10@gmail.com>
2024-06-19 20:59:38 -0300

    Fibonacci refactoring

--- Algorithms/RecursiveFibonacci/Program.cs
+++ Algorithms/RecursiveFibonacci/Program.cs
@@ -5,18 +5,13 @@
         //1 1 2 3 5 8 13 21
         static void Main()
         {
-            var position = 6;
+            ulong position = 6;
             var result = Fibonacci(position);
             Console.WriteLine($"Position {position}: {result}");
 
             Console.ReadKey();
         }
 
-        public static long Fibonacci(long position)
-        {
-            if (position <= 0) { return 0; }
-            if (position <= 2) { return 1; }
-            return Fibonacci(position - 2) + Fibonacci(position - 1);
-        }
+        public static ulong Fibonacci(ulong position) => position < 2 ? position : Fibonacci(position - 2) + Fibonacci(position - 1);
     }
 }
