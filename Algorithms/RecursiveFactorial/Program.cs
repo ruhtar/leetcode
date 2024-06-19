5143d1886ea298b52775565ba6eb2cf4e6fdefbb
ruhtar <arthur.amorim10@gmail.com>
2024-06-19 20:51:44 -0300

    Adds Recursive functions

---
+++ Algorithms/RecursiveFactorial/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RecursiveFactorial;
+
+internal class Program
+{
+    static void Main()
+    {
+        int number = 5;
+        int result = CalculateFactorial(number);
+
+        Console.WriteLine($"Factorial of {number} is {result}");
+
+        Console.ReadKey();
+    }
+    public static int CalculateFactorial(int num)
+    {
+        if (num < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(num), "Number must be non-negative.");
+        }
+
+        if (num == 0) return 1;
+
+        return num * CalculateFactorial(num - 1);
+    }
+}
