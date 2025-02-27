d04095309b8e361c96c77e24171265eaff58d7ba
ruhtar <arthur.amorim10@gmail.com>
2025-02-27 20:19:48 -0300

    adds BubbleSort

---
+++ Algorithms/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BubbleSort;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new int[] { 6, 12, 62, 13, 789, 234, 1212, 2, 40, 10, 1000, 200, 333, -4, 0 };
+        var sortedList = Program.BubbleSort(list);
+        foreach (var item in sortedList)
+        {
+            Console.WriteLine(item);
+        };
+        Console.ReadLine();
+    }
+
+    public static int[] BubbleSort(int[] list)
+    {
+        while (true) 
+        {
+            var switched = false;
+
+            for (var i = 0; i < list.Length - 1; i++)
+            {
+                var first = i;
+                var second = i + 1;
+                if (list[first] > list[second])
+                {
+                    var temp = list[first];
+                    list[first] = list[second];
+                    list[second] = temp;
+                    switched = true;
+                }
+            }
+
+            if (!switched) 
+            {
+                break;
+            }
+        };
+
+        return list;    
+    }
+}
